4a2645a85c2440d2e4ca32d1db0245b304858071
Kyle <kylepw@gmail.com>
2019-04-05 00:17:47 +0900

    Update docs to reflect v0.1.0 release.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Kyle Weeks'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
